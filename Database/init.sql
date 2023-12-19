1634e50709df57a6b6d060f2f60fa672637627db
def <basc11111@gmail.com>
2023-12-19 18:53:58 +0300

    add init database

---
+++ Database/init.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE students
+(
+    id            serial PRIMARY KEY,
+    student_name  text NOT NULL,
+    study_group   text NOT NULL,
+    date_of_birth date NOT NULL
+);
+
+COPY students (id, student_name, study_group, date_of_birth)
+    FROM '/data/database/students.csv'
+    DELIMITER ','
+    CSV HEADER;
